153144ba21ec08ae38fd05efb606155f2e37d76d
rhf007 <rahofahazem512@gmail.com>
2026-06-23 12:23:22 +0300

    make login button reusable for login and register

--- src/renderer/src/components/LoginButton.jsx
+++ src/renderer/src/components/LoginButton.jsx
@@ -1,11 +1,11 @@
-export default function LoginButton() {
+export default function LoginButton({ children }) {
   return (
     <div>
       <button
         type="submit"
         className="flex w-full justify-center rounded-md bg-[#b31313] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-[#800d0d] hover:cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b31313]"
       >
-        Sign in
+        {children}
       </button>
     </div>
   )
