973ee347139f53c1d4326b1cc83a104edf751a88
rhf007 <rahofahazem512@gmail.com>
2026-06-25 18:17:09 +0300

    login backend complete

--- src/main/index.js
+++ src/main/index.js
@@ -1,29 +1,29 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
+import axios from 'axios'
+import https from 'https'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+
+let BASE_URL = import.meta.env.MAIN_VITE_FRAPPE_URL
+let cookieHeader = ''
 
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
+    width: 1200,
+    height: 800,
+    minWidth: 950,
+    minHeight: 750,
+    //     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      preload: join(__dirname, '../preload/index.js')
+      //       sandbox: false
     }
   })
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-  })
-
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
   })
 
   // HMR for renderer base on electron-vite cli.
@@ -35,12 +35,14 @@ function createWindow() {
   }
 }
 
+//Electron extra config. TODO: uncomment when done
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  //Electron extra config. TODO: uncomment when done
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  // electronApp.setAppUserModelId('com.electron')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
@@ -49,26 +51,83 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
   createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  ipcMain.handle('login', async (_, { email, password }) => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/method/login`,
+        new URLSearchParams({
+          usr: email,
+          pwd: password
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+          })
+        }
+      )
+
+      const cookies = response.headers['set-cookie']
+
+      cookieHeader = cookies.map((c) => c.split(';')[0]).join('; ')
+
+      const loggedUser = await axios.get(`${BASE_URL}/method/frappe.auth.get_logged_user`, {
+        headers: {
+          Cookie: cookieHeader
+        },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false
+        })
+      })
+
+      return {
+        success: true,
+        user: loggedUser.data.message
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: error.response?.data.message || error.message
+      }
+    }
+  })
+
+  ipcMain.handle('get-users', async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/resource/User`, {
+        headers: {
+          Cookie: cookieHeader
+        },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false
+        })
+      })
+
+      return response.data
+    } catch (error) {
+      throw new Error(error.message)
+    }
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+//TODO: uncomment when done
+//extra config by electron
+//   app.on('activate', function () {
+//     // On macOS it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// // Quit when all windows are closed, except on macOS. There, it's common
+// // for applications and their menu bar to stay active until the user quits
+// // explicitly with Cmd + Q.
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
