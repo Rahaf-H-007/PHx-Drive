8d80d9fe48811f6953ffdb13f19cd19f5851ae69
rhf007 <rahofahazem512@gmail.com>
2026-06-27 18:15:22 +0300

    Setup routing

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-// import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       {/* <LoginPage /> */}
-      <Home />
+      {/* <Home /> */}
     </>
   )
 }
